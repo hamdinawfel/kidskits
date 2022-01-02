9f227f8c36668f856e7d3c2ee57659cf748af14c
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-01-02 16:17:55 +0100

    build : add like and delete features for posts and comments

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import MenuBar from "./shared/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import SinglePost from "./pages/SinglePost";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route exact path='/' component={Home} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
+            <Route exact path='/posts/:postId' component={SinglePost} />
           </Container>
         </Switch>
       </Router>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useQuery } from "@apollo/client";
-import { Grid } from "semantic-ui-react";
+import { Grid, Transition } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 import PostCard from "../shared/PostCard";
@@ -25,12 +25,14 @@ function Home() {
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
-          data.getPosts &&
-          data.getPosts.map((post) => (
-            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-              <PostCard post={post} />
-            </Grid.Column>
-          ))
+          <Transition.Group>
+            {data.getPosts &&
+              data.getPosts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
         )}
       </Grid.Row>
     </Grid>
